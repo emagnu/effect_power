c7449efd9233e2b8973609c4842029af085f1b89
Emmanuele <emagnubcp@gmail.com>
2024-05-28 20:53:45 +0100

    Starting code

---
+++ lib/core_src/presentation/pages/home_page.dart
@@ -0,0 +1,15 @@
+//  //   ///
+//  Import LIBRARIES
+import 'package:flutter/material.dart';
+
+//  Import  FILES
+//  //   ///
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
